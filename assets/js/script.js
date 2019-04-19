5179f8219bd8a9fe39e0fa9a883d84ea3d88cd1f
AndyRojas <andyrojasucr@gmail.com>
2019-04-18 22:52:55 -0600

    New img

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
 var lang = {
-        "html": "95%",
-        "css": "85%",
-        "javascript": "80%",
-        "php": "80%",
-        "angulars": "60%",
-        "cyber": "50%",
-        "react": "65%",
-        "vue": "75%",
-        "firebase": "80%",
-        "bd": "80%",
-        "drupal": "50%",
-        "android": "84%",
-        "csharp": "78%",
-        "java": "90%",
-        "wordpress": "90%"
-        };
+    "html": "95%",
+    "css": "85%",
+    "javascript": "80%",
+    "php": "80%",
+    "angulars": "60%",
+    "cyber": "50%",
+    "react": "65%",
+    "vue": "75%",
+    "firebase": "60%",
+    "bd": "60%",
+    "drupal": "50%",
+    "android": "80%",
+    "csharp": "75%",
+    "java": "80%",
+    "wordpress": "80%"
+};
 
-        var multiply = 4;
+var multiply = 4;
 
-        $.each( lang, function( language, pourcent) {
+$.each(lang, function(language, pourcent) {
 
-        var delay = 0;
-        
-        setTimeout(function() {
-            $('#'+language+'-pourcent').html(pourcent);
-        },delay*multiply);
-        
-        multiply++;
+    var delay = 0;
 
-        });
+    setTimeout(function() {
+        $('#' + language + '-pourcent').html(pourcent);
+    }, delay * multiply);
+
+    multiply++;
+
+});
